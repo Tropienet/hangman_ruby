b61c7a2ee4daa79fb62c8be628289270772244fa
Tin Goricki <exibodude@gmail.com>
2023-04-06 22:57:01 +0200

    Added game class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,8 +2,45 @@ puts "Hangman initialized"
 
 lines = File.readlines("words.txt")
 
+letters = "qwertzuiopasdfghjklyxcvbnm"
+
+class Game 
+    def initialize(lines)
+        @lines = lines
+        @lives = 5;
+        @guesses = []
+        @word_list = []
+        @word = ""
+        @letters = "qwertzuiopasdfghjklyxcvbnm"
+    end
+
+    def get_word_list 
+        @lines.each do |line|
+            if line.length > 5 && line.length < 12
+                @word_list.push(line)
+            end
+        end
+    end
+
+    def generate_random_word
+        @word = @word_list[rand(@word_list.length)]
+    end
+
+    def play_round(guess)
+        if @word.include?(guess) 
+            puts "The word includes #{guess}"
+        end
+    end
+end
+
+game = Game.new(lines)
+game.get_word_list
+game.generate_random_word
+game.play_round(gets.chomp)
+
 
 word_list = []
+lives = 5
 
 lines.each do |line|
     if line.length > 5 && line.length < 12
@@ -12,5 +49,46 @@ lines.each do |line|
 end
 
 word = word_list[rand(word_list.length)]
+guesses = []
+current_word = word
 
-puts word
+while lives > 0 do
+    alreadyTried = true
+
+    def getGuess 
+        puts "Please enter 1 letter"
+        return gets.chomp
+    end
+
+    guess = getGuess()
+
+    while alreadyTried do
+        if guesses.include?(guess)
+            if lives > 0
+                lives -= 1
+                puts "You already tried this letter, current lives #{lives}"
+                guess = getGuess()
+            end
+        elsif
+            alreadyTried = false
+        end
+    end
+    while guess.length > 1 do 
+        guess = getGuess
+    end 
+
+    if word.include?(guess)
+        letters = letters.gsub(guess, "_")
+        puts letters
+        puts "This is the word #{current_word.gsub(letters, "_")}"
+        guesses.push(guess)
+        p "Already tried #{guesses.join(",")}"
+    elsif
+        lives -=1
+        puts "Remaining lives #{lives}"
+        guesses.push(guess)
+        p "Already tried #{guesses.join(",")}"
+    end
+
+    
+end
