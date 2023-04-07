ece05ae94c0cf5b28262a19be9e07308a3ce2565
Tin Goricki <exibodude@gmail.com>
2023-04-07 15:56:40 +0200

    Cleaned up code with rubocop and cleaned up display

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -29,6 +29,7 @@ class Game
       puts "The word includes #{guess}"
     else
       @lives -= 1
+      puts "Remaining lives #{@lives}"
     end
     @guesses.push(guess)
   end
@@ -37,9 +38,22 @@ class Game
     @lives
   end
 
+  def display_guesses
+    remaining_letters = @letters.split('')
+    remaining_letters.pop
+    puts "Letters you already tried guessing \n"
+    remaining_letters.each do |letter|
+      if @guesses.include?(letter)
+        print letter
+      else
+        print ' _ '
+      end
+    end
+  end
+
   def display_word
-    current_word = @word.split("")
-    current_word.pop()
+    current_word = @word.split('')
+    current_word.pop
     current_word.each do |letter|
       if @guesses.include?(letter)
         print letter
@@ -47,25 +61,27 @@ class Game
         print ' _ '
       end
     end
-    if (current_word - @guesses).empty?
-        puts "You win"
-        @lives = 0
-    end
+    return unless (current_word - @guesses).empty?
+
+    puts 'You win'
+    @lives = 0
   end
 end
 
 game = Game.new(lines)
 game.get_word_list
 game.generate_random_word
-game.display_word
 
-while !game.return_lives.zero?
-  puts 'please enter a letter'
+until game.return_lives.zero?
+  puts "\nEnter a letter"
   guess = gets.chomp
   while guess.length > 1
-    puts 'please enter a letter'
+    puts "\nEnter a letter"
     guess.gets.chomp
   end
   game.play_round(guess)
   game.display_word
+  puts "\n"
+  game.display_guesses
+  puts "\nYou lose" if game.return_lives.zero?
 end
